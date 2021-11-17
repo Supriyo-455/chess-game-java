bc9baca549cb5a522bdfe7fef92afff296b9b47e
supriyo <paulsupriyo64nk@gmail.com>
2021-11-17 09:04:38 +0530

    Pawn piece basic

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -9,16 +9,20 @@ import java.util.Collection;
 public abstract class Piece {
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
 
     public Piece(final int piecePosition,final Alliance pieceAlliance) {
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
+        //TODO: work here
+        this.isFirstMove = false;
     }
 
     public Alliance getPieceAlliance(){
         return this.pieceAlliance;
     }
-
+    public boolean isFirstMove(){
+        return this.isFirstMove;
+    }
     public abstract Collection<Move> calculateLegalMoves(final Board board);
-
 }
