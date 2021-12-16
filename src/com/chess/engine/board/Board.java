d2cea5006009acfadeae32a68fea23bad8384ab4
supriyo <paulsupriyo64nk@gmail.com>
2021-12-16 11:25:16 +0530

    move part 4

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -6,6 +6,7 @@ import com.chess.engine.player.BlackPlayer;
 import com.chess.engine.player.Player;
 import com.chess.engine.player.WhitePlayer;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 
 import java.util.*;
 
@@ -149,6 +150,10 @@ public class Board {
     public Tile getTile(final int coordinate) {
 
         return this.gameBoard.get(coordinate);
+    }
+
+    public Iterable<Move> getAllLegalMoves() {
+        return Iterables.unmodifiableIterable(Iterables.concat(this.whitePlayer.getLegalMoves(), this.blackPlayer.getLegalMoves()));
     }
 
     public static class Builder {
