a3e4b945ae696b260cf1f2a8ddb838946ef7df17
supriyo <paulsupriyo64nk@gmail.com>
2021-12-20 20:40:24 +0530

    GUI part 4

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -1,21 +1,26 @@
 package com.chess.gui;
 
+import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Table {
 
-    private final Color lightTileColor = Color.decode("#FFFACD");
-    private final Color darkTileColor = Color.decode("#593E1A");
     private static final Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
     private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
     private static final Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
+    private final Color lightTileColor = Color.decode("#FFFACD");
+    private final Color darkTileColor = Color.decode("#593E1A");
     private final JFrame gameFrame;
     private final BoardPanel boardPanel;
     private final JMenuBar tableMenuBar;
@@ -88,16 +93,36 @@ public class Table {
             validate();
         }
 
+        private void assignTilePieceIcon(final Board board) {
+            this.removeAll();
+            if (board.getTile(this.tileId).isTileOccupied()) {
+                String pieceIconPath = "";
+                try {
+                    final BufferedImage image = ImageIO.read(
+                            new File(
+                                    pieceIconPath +
+                                            board.getTile(this.tileId).getPiece().getPieceAlliance().toString().substring(0, 1) +
+                                            board.getTile(this.tileId).getPiece().toString() +
+                                            ".gif"
+                            )   //For example pieceIconPath+"W"+"B"+".gif"
+                    );
+                    add(new JLabel(new ImageIcon(image)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         private void assignTileColor() {
-            if (BoardUtils.FIRST_ROW[this.tileId] ||
-                    BoardUtils.THIRD_ROW[this.tileId] ||
-                    BoardUtils.FIFTH_ROW[this.tileId] ||
-                    BoardUtils.SEVENTH_ROW[this.tileId]) {
+            if (BoardUtils.EIGHTH_RANK[this.tileId] ||
+                    BoardUtils.SIXTH_RANK[this.tileId] ||
+                    BoardUtils.FOURTH_RANK[this.tileId] ||
+                    BoardUtils.SECOND_RANK[this.tileId]) {
                 setBackground(this.tileId % 2 == 0 ? lightTileColor : darkTileColor);
-            } else if (BoardUtils.SECOND_ROW[this.tileId] ||
-                    BoardUtils.FOURTH_ROW[this.tileId] ||
-                    BoardUtils.SIXTH_ROW[this.tileId] ||
-                    BoardUtils.EIGHTH_ROW[this.tileId]) {
+            } else if (BoardUtils.SEVENTH_RANK[this.tileId] ||
+                    BoardUtils.FIFTH_RANK[this.tileId] ||
+                    BoardUtils.THIRD_RANK[this.tileId] ||
+                    BoardUtils.FIRST_RANK[this.tileId]) {
                 setBackground(this.tileId % 2 != 0 ? lightTileColor : darkTileColor);
             }
         }
